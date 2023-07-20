0501e7d403c8531c9cc5f8f8174b69f21392029a
Henry Megwai <henrymegwai@gmail.com>
2023-07-20 08:51:41 +0200

    Add project files.

---
+++ ServiceBusManagementConsole/ManagementConsole.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ServiceBusManagementConsole
+{
+    internal class ManagementConsole
+    {
+        // Enter a valid Service Bus connection string
+        private static string ServiceBusConnectionString = "copy and paste the connection string of your service bus. This bad practise though, however just for test purpose";
+
+
+        static async Task Main(string[] args)
+        {
+            ManagementHelper helper = new ManagementHelper(ServiceBusConnectionString);
+
+
+
+
+            bool done = false;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write(">");
+                string commandLine = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                string[] commands = commandLine.Split(' ');
+
+                try
+                {
+                    if (commands.Length > 0)
+                    {
+                        switch (commands[0])
+                        {
+                            case "createqueue":
+                            case "cq":
+                                if (commands.Length > 1)
+                                {
+                                    await helper.CreateQueueAsync(commands[1]);
+                                }
+                                else
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Yellow;
+                                    Console.WriteLine("Queue name not specified.");
+                                }
+                                break;
+                            case "listqueues":
+                            case "lq":
+                                helper.ListQueuesAsync().Wait();
+                                break;
+                            case "getqueue":
+                            case "gq":
+                                if (commands.Length > 1)
+                                {
+                                    await helper.GetQueueAsync(commands[1]);
+                                }
+                                else
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Yellow;
+                                    Console.WriteLine("Queue name not specified.");
+                                }
+                                break;
+                            case "deletequeue":
+                            case "dq":
+                                if (commands.Length > 1)
+                                {
+                                    await helper.DeleteQueueAsync(commands[1]);
+                                }
+                                else
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Yellow;
+                                    Console.WriteLine("Queue name not specified.");
+                                }
+                                break;
+                            case "createtopic":
+                            case "ct":
+                                if (commands.Length > 1)
+                                {
+                                    await helper.CreateTopicAsync(commands[1]);
+                                }
+                                else
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Yellow;
+                                    Console.WriteLine("Topic name not specified.");
+                                }
+                                break;
+                            case "createsubscription":
+                            case "cs":
+                                if (commands.Length > 2)
+                                {
+                                    await helper.CreateSubscriptionAsync(commands[1], commands[2]);
+                                }
+                                else
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Yellow;
+                                    Console.WriteLine("Topic name not specified.");
+                                }
+                                break;
+                            case "listtopics":
+                            case "lt":
+                                await helper.ListTopicsAndSubscriptionsAsync();
+                                break;
+                            case "help":
+                                Console.WriteLine("cq createqueue [queueName]");
+                                Console.WriteLine("lq listqueues");
+                                Console.WriteLine("gq getqueue [queueName]");
+                                Console.WriteLine("dq deletequeue");
+                                Console.WriteLine("ct createtopic [topicName]");
+                                Console.WriteLine("cs createsubscription [topicName] [subscriptionName]");
+                                Console.WriteLine("lt listtopics");
+      
+                                break;
+                            case "exit":
+                                done = true;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(ex.Message);
+                }
+
+            } while (!done);
+        }
+    }
+}
---
+++ ServiceBusManagementConsole/ManagementHelper.cs
@@ -0,0 +1,129 @@
+﻿using Azure.Messaging.ServiceBus.Administration;
+using System;
+using System.Threading.Tasks;
+
+
+
+namespace ServiceBusManagementConsole
+{
+    internal class ManagementHelper
+    {
+
+
+        private ServiceBusAdministrationClient m_AdministrationClient;
+
+        public ManagementHelper(string connectionString)
+        {
+            // Create a ServiceBusAdministrationClientu using the specified credentials.
+            m_AdministrationClient = new ServiceBusAdministrationClient(connectionString);
+        }
+
+
+
+        public async Task CreateQueueAsync(string queueName)
+        {
+            Console.Write("Creating queue {0}...", queueName);
+            var createQueueOptions = GetCreateQueueOptions(queueName);
+            var response = await m_AdministrationClient.CreateQueueAsync(createQueueOptions);
+            var queueProperties = response.Value;
+
+            Console.WriteLine("Done!");
+        }
+
+        public async Task DeleteQueueAsync(string queueName)
+        {
+            Console.Write("Deleting queue {0}...", queueName);
+            await m_AdministrationClient.DeleteQueueAsync(queueName);
+
+            Console.WriteLine("Done!");
+        }
+
+        public async Task ListQueuesAsync()
+        {
+            var queuePropertiesList = m_AdministrationClient.GetQueuesAsync();
+            Console.WriteLine("Listing queues...");
+            await foreach (var queueProperties in queuePropertiesList)
+            {
+                Console.WriteLine("\t{0}", queueProperties.Name);
+            }
+            Console.WriteLine("Done!");
+        }
+
+        public async Task GetQueueAsync(string queueName)
+        {
+            var response = await m_AdministrationClient.GetQueueAsync(queueName);
+            var queueProperties = response.Value;
+            Console.WriteLine($"Queue description for { queueName }");
+            Console.WriteLine($"    Name:                                { queueProperties.Name }");
+            Console.WriteLine($"    MaxSizeInMegabytes:                  { queueProperties.MaxSizeInMegabytes }");
+            Console.WriteLine($"    RequiresSession:                     { queueProperties.RequiresSession }");
+            Console.WriteLine($"    RequiresDuplicateDetection:          { queueProperties.RequiresDuplicateDetection }");
+            Console.WriteLine($"    DuplicateDetectionHistoryTimeWindow: { queueProperties.DuplicateDetectionHistoryTimeWindow }");
+            Console.WriteLine($"    LockDuration:                        { queueProperties.LockDuration }");
+            Console.WriteLine($"    DefaultMessageTimeToLive:            { queueProperties.DefaultMessageTimeToLive }");
+            Console.WriteLine($"    DeadLetteringOnMessageExpiration:    { queueProperties.DeadLetteringOnMessageExpiration }");
+            Console.WriteLine($"    EnableBatchedOperations:             { queueProperties.EnableBatchedOperations }");
+            Console.WriteLine($"    MaxDeliveryCount:                    { queueProperties.MaxDeliveryCount }");
+            Console.WriteLine($"    Status:                              { queueProperties.Status }");
+        }
+
+
+
+        public async Task CreateTopicAsync(string topicName)
+        {
+            Console.Write("Creating topic {0}...", topicName);
+            var response = await m_AdministrationClient.CreateTopicAsync(topicName);
+            var topicProperties = response.Value;
+
+            Console.WriteLine("Done!");
+        }
+
+
+        public async Task CreateSubscriptionAsync(string topicName, string subscriptionName)
+        {
+            Console.Write("Creating subscription {0}/subscriptions/{1}...", topicName, subscriptionName);
+            var response = await m_AdministrationClient.CreateSubscriptionAsync(topicName, subscriptionName);
+            var subscriptionProperties = response.Value;
+
+            Console.WriteLine("Done!");
+        }
+
+
+        public async Task ListTopicsAndSubscriptionsAsync()
+        {
+            var topicPropertiesList = m_AdministrationClient.GetTopicsAsync();
+            Console.WriteLine("Listing topics and subscriptions...");
+            await foreach (var topicProperties in topicPropertiesList)
+            {
+                Console.WriteLine("\t{0}", topicProperties.Name);
+                var subscriptionPropertiesList = m_AdministrationClient.GetSubscriptionsAsync(topicProperties.Name);
+                await foreach (var subscriptionProperties in subscriptionPropertiesList)
+                {
+                    Console.WriteLine("\t\t{0}", subscriptionProperties.SubscriptionName);
+                }
+            }
+            Console.WriteLine("Done!");
+        }
+
+
+
+
+        public CreateQueueOptions GetCreateQueueOptions(string queueName)
+        {
+
+            
+
+            return new CreateQueueOptions(queueName)
+            {
+                RequiresDuplicateDetection = true,
+                DuplicateDetectionHistoryTimeWindow = TimeSpan.FromMinutes(10),
+                RequiresSession = true,
+                MaxDeliveryCount = 20,
+                DefaultMessageTimeToLive = TimeSpan.FromHours(1),
+                DeadLetteringOnMessageExpiration = true
+            };
+        }
+
+
+    }
+}
